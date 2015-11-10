0e19d3e8962cb63134b0f366e90d2b4cd236901e
Goetz Lehmann <goetz@tp1.uni-duesseldorf.de>
2015-11-10 19:05:46 +0100

    Example solution

--- cmplx_func.cxx
+++ cmplx_func.cxx
@@ -1,13 +1,35 @@
+// Complex functions
+// GL, 5.5.14
 #include <iostream>
+#include <fstream>
 #include <complex>
 
-int main(){
+using namespace std;
 
-  // Example how to declare a single complex variable:
-  complex<double> c;
+// Prototype
+complex<double> f(double phi);
 
-  // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
+int main()
+{
+    const int N = 100;
+    complex<double> z[N];
+    const double dphi = 2*M_PI/N;
+    ofstream out("output");
 
-  return 0;
+    for(int i=0; i<N; i++)
+	   z[i] = f(dphi*i-M_PI);
+
+    for(int i=0; i<N; i++)
+      out << i*dphi << "\t" << real(z[i]) << "\t"
+          << imag(z[i]) << "\t"  << abs(z[i]) << endl;
+
+    out.close();
+
+    return 0;
+}
+
+
+complex<double> f(double phi)
+{
+   return( tan(phi * complex<double>(1,1)) );
 }
